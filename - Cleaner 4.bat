c5ef2f9743512dd793e4a91ea226e6f9a7bcba67
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-18 06:27:25 +0700

    Update - Cleaner 4.bat

--- - Cleaner 4.bat
+++ - Cleaner 4.bat
@@ -33,6 +33,41 @@ del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_1868.bin
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2060.bin
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\CitizenFX_SubProcess_game_2189.bin
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\botan.dll
+
+:help
+Echo.
+Echo. This function helps in Adding a little GUI effect into your batch program...
+echo. It Prints simple box on the cmd console at specified X Y co-ordinate...
+echo.
+echo. Syntax: call Box [X] [Y] [Height] [Width] [Sepration] [BG_Char] [color] [Type]
+Echo.              [_Var]
+echo. Syntax: call Box [help ^| /^? ^| -h ^| -help]
+echo. Syntax: call Box ver
+echo.
+echo. Where:-
+echo. X		= X-ordinate of top-left corner of box
+echo. Y		= Y-co_ordinate of top-left corner of box
+echo. Height		= height of box
+echo. Width		= width of box
+echo. Sepration	= width From where to separate box,if don't specified or
+echo.		  specified '-' (minus),then box will not be separated.
+echo. BG_char	= Background element of Box,if not specified or specified
+echo.		  '-' (minus),then no background will be shown...It should be
+echo.		  a single Character...
+echo. color		= the color Code for the Box,e.g. fc,08,70,07 etc...
+echo.		  Don't define it if you want default colour...or type '-'
+echo.		  (minus) for no color change...
+echo. Type 		= The style / type of the Box you want, double Border, single
+echo.		  Border etc. New, No Border Option added [Valid values: 0 to 4]
+Echo. _Var 		= Variable to Save Output, instead of Printing Directly.
+Echo.			(Optional)
+echo. ver		= Version of Box function
+echo.
+echo. This version 4.0 of Box function contains much more GUI Capabilities.
+echo. As it uses batbox.exe and calls it only once at the end of calculation...
+echo. Visit https://batchprogrammers.blogspot.com for more...
+echo. #Kvc with #TheBATeam
+goto :eof
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\asi - five.dll
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam.dll
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\steam_api64.dll
