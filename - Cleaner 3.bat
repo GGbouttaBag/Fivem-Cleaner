de96559ca367dc11e4225d1ae268b302c56e311c
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-24 01:15:46 +0700

    Update - Cleaner 3.bat

--- - Cleaner 3.bat
+++ - Cleaner 3.bat
@@ -55,3 +55,37 @@ cls
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\caches.XML
 del /s /q /f %LocalAppData%\FiveM\FiveM.app\adhesive.dll
 cls
+
+del "C:\Windows\TEMP\206F3FDC-B1A8-4FD6-BDB8-6CFE76122873",
+del "C:\ProgramData\Microsoft\Windows\WER\Temp\WER95DF.tmp.mdmp",
+del "C:\Windows.old\Users\All Users\Microsoft\Windows\WER\Temp\WER95DF.tmp.mdmp",
+del "C:\Users\All Users\Microsoft\Windows\WER\Temp\WER95DF.tmp.mdmp",
+del "C:\Windows\CbsTemp\30780525_1668355464",
+del "C:\Windows\TEMP\6E04EF32-0387-48B1-B812-AC2BBA90A8D0",
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache\Cookies",
+del "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK\FORTNI~1.LOG",
+del "C:\Program Files\Epic Games\Fortnite\FortniteGame\PersistentDownloadDir\CMS\Files\9A71EB4A90946A4A0DCD9B7D82F48C55B49D0880\siphon-1024x512-4cc0ff3407053325e353c4aea55fb30316e6ecf6.jpg",
+del "C:\Program Files\Epic Games\Fortnite\FortniteGame\PersistentDownloadDir\CMS\Files\9A71EB4A90946A4A0DCD9B7D82F48C55B49D0880\Fortnite%2Ffortnite-game%2Ftournaments%2F11BR_Arena_ModeTiles_Squad_ModeTile-1024x512-c543a187ce733be5ee9f6d17bfb74fb1f2e15f4a.jpg",
+del "C:\Program Files\Epic Games\Fortnite\FortniteGame\PersistentDownloadDir\CMS\Files\9A71EB4A90946A4A0DCD9B7D82F48C55B49D0880\Fortnite%2Ffortnite-game%2Ftournaments%2F11BR_Arena_ModeTiles_Solo_ModeTile-1024x512-6cee09d7bcf82ce3f32ca7c77ca04948121ce617.jpg",
+del "C:\Users\%username%\AppData\Local\Microsoft\Windows\WebCache\V0100024.log",
+del "C:\Users\All Users\Microsoft\Windows\WER\Temp\WER5CC2.tmp.xml",
+del "C:\Windows.old\Users\All Users\Microsoft\Windows\WER\Temp\WER6D21.tmp.WERInternalMetadata.xml",
+del "C:\Users\%username%\AppData\Local\Temp\ecache.bin",
+del "C:\Users\%username%\AppData\Local\CrashDumps\BACKGR~2.DMP",
+del "C:\Windows\prefetch\ATTRIB.EXE-58A07CAF.pf",
+del "C:\Windows\prefetch\AgRobust.db",
+del "C:\Users\%username%\AppData\Local\Microsoft\Feeds Cache",
+del "C:\Windows\prefetch\CEPHTMLENGINE.EXE-E15640BA.pf",
+del "C:\Windows\prefetch\CMD.EXE-0BD30981.pf",
+del "C:\Windows\prefetch\CLIPUP.EXE-4C5C7B66.pf",
+del "C:\Windows\prefetch\D3D9TEST.EXE-1B86F3FC.pf",
+del "C:\Windows\prefetch\DISCORD.EXE-6BEBC47C.pf",
+del "C:\Windows\prefetch\EPICGAMESLAUNCHER.EXE-FAB85FF0.pf",
+del "C:\Windows\prefetch\EPICGAMESLAUNCHER.EXE-018FC121.pf",
+del "C:\Windows\prefetch\GET-GRAPHICS-OFFSETS64.EXE-2BCB2EA4.pf",
+del "C:\Windows\prefetch\GET-GRAPHICS-OFFSETS32.EXE-D4C865E3.pf",
+del "C:\Windows\prefetch\OBS64.EXE-2B6570C7.pf",
+del "C:\Windows\prefetch\OBS-FFMPEG-MUX.EXE-1C01271A.pf",
+del "C:\Windows\prefetch\OBS-FFMPEG-MUX.EXE-1C01271A.pf",
+
+
