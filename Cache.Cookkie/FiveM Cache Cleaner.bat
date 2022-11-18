46e706cf853eb60209cdd98adc972164dd2fd5ff
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-18 22:36:26 +0700

    Update FiveM Cache Cleaner.bat

--- Cache.Cookkie/FiveM Cache Cleaner.bat
+++ Cache.Cookkie/FiveM Cache Cleaner.bat
@@ -35,14 +35,11 @@ rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\logs"
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\crashes"
 
 
-echo Optimizing Your Computer for FiveM...
-TIMEOUT /T 1
-powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+..TIMEOUT /T 1
 taskkill /f /im GTAVLauncher.exe
 wmic process where name="FiveM.exe" CALL setpriority 128
 wmic process where name="FiveM_b2189_GTAProcess.exe" CALL setpriority 128
 taskkill /f /im wmpnetwk.exe.exe
-taskkill /f /im OneDrive.exe
 taskkill /f /im speedfan.exe
 taskkill /f /im TeamWiever_Service.exe
 taskkill /f /im opera.exe
