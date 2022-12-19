d27c5619c4ce2cc9e5a523831c0b33c84d9d456e
Slackes <SarnaxOfficial@protonmail.com>
2022-12-20 01:45:28 +0700

    Update RemoveLog.bat

--- Clean.Server/RemoveLog.bat
+++ Clean.Server/RemoveLog.bat
@@ -1,9 +1,10 @@
 @echo off
 title FiveM Cleaner ::: KALASH LUA ::: discord.gg/BukSCSnDjn
+
+:: Set the console text color to bright yellow
 color e
 
-
-
+:: Display the title and logo
 echo "______           _                    "
 echo "|  ____|         | |                  "
 echo "| |__ _ __   ___ | |__   ___ _ __ ____"
@@ -11,22 +12,28 @@ echo "|  __| '_ \ / _ \| '_ \ / _ \ '__|_  /"
 echo "| |  | | | | (_) | |_) |  __/ |   / / "
 echo "|_|  |_| |_|\___/|_.__/ \___|_|  /___|"
 echo. 
+
+:: Prompt the user to press any key to continue
 echo Pritisnite bilo koju tipku za brisanje FiveM predmemorije.
 pause >nul
+
+:: Clear the console window
 cls
 
-
-color c
+:: Display a message indicating that the cache is being deleted
 echo Brisanje FiveM predmemorije...
-TIMEOUT /T 5 /nobreak
+
+:: Delete the cache directories
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\cache"
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache"
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache-priv"
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\logs"
 rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\crashes"
 
+:: Wait for a short period of time
+timeout /t 1
 
-TIMEOUT /T 1
+:: Clear the console window and display a message indicating that the cache has been cleaned
 color e
 cls
 echo "______           _                    "
@@ -38,6 +45,7 @@ echo "|_|  |_| |_|\___/|_.__/ \___|_|  /___|"
 echo. 
 echo Ociscena FiveM predmemorija!
 echo.
-echo.
+
+:: Prompt the user to press any key to exit the script
 echo Pritisnite bilo koju tipku za zatvorite program.
 pause >nul
